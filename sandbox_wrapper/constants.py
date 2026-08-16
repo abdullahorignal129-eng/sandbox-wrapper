5cd85a19b9931f0a975b000ab0c85527b1ffe663
abdullahorignal129-eng <abdullahorignal129@gmail.com>
2026-08-16 09:49:56 +0500

    Update constants.py

--- sandbox_wrapper/constants.py
+++ sandbox_wrapper/constants.py
@@ -3,6 +3,6 @@ from pathlib import Path
 DEFAULT_SHARED_FOLDER = Path(r"G:\Archnemix\Dataset\Shared")
 DEFAULT_WSB_FOLDER = Path(r"G:\Archnemix\Dataset")
 
-SERVER_START_TIMEOUT = 180   # seconds
-TASK_TIMEOUT = 120
+SERVER_START_TIMEOUT = 180  # seconds
+TASK_TIMEOUT = 300          # bumped up: venv creation + pip install + run can take longer than 120s
 POLL_INTERVAL = 0.5
